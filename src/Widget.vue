98310850f453858bd2d5f0665a8577a5aeaf2914
aziis98 <antonio.delucreziis@gmail.com>
2017-08-19 23:26:11 +0200

    Initial commit

---
+++ src/Widget.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <div class="widget" :style="{ width: width + 'px', height: height + 'px', left: left + 'px', top: top + 'px' }">
+    <!-- Stuff, slots or something -->
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'widget',
+        data: () => ({
+            width: 300,
+            height: 200,
+            left: 0,
+            top: 0,
+        }),
+    }
+</script>
+
+<style lang="sass">
+
+    .widget
+        border: 1px solid #333
+        border-radius: 4px
+        position: absolute
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+
+import App from './App.vue'
+import Widget from './Widget.vue'
+
+new Vue({
+    el: '#app',
+
+    data: {
+        
+    },
+
+    render: h => h(App),
+});
